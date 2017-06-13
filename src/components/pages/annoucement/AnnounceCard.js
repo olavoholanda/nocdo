59d8e70c7bd3db7e7a01e2744a4c0aa8fc61d470
Olavo Holanda <olavohcn@gmail.com>
2017-06-13 11:44:57 -0300

    Added some semicolons for terminating statements
    Still figuring out when the semicolons are needed in react

--- src/components/pages/annoucement/AnnounceCard.js
+++ src/components/pages/annoucement/AnnounceCard.js
@@ -1,13 +1,13 @@
 import React, {Component} from 'react'
-import {Card, CardHeader, CardText} from 'material-ui/Card';
-import PropTypes from 'prop-types';
-import {deepOrangeA400} from "material-ui/styles/colors";
+import {Card, CardHeader, CardText} from 'material-ui/Card'
+import PropTypes from 'prop-types'
+import {deepOrangeA400} from 'material-ui/styles/colors'
 
 class AnnounceCard extends Component {
 
     render() {
-        const {announcement} = this.props
-        let subtitle = "Postado em " + announcement.date
+        const {announcement} = this.props;
+        let subtitle = "Postado em " + announcement.date;
         return (
             <Card style={{margin: 15}}>
                 <CardHeader titleColor={deepOrangeA400} titleStyle={{fontWeight: "bold"}} title={announcement.title}
@@ -23,6 +23,6 @@ class AnnounceCard extends Component {
 
 AnnounceCard.propTypes = {
     announcement: PropTypes.object.isRequired
-}
+};
 
 export default AnnounceCard
