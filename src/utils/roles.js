2f9821773df44dce1a60ceaa69e14e813a212219
Olavo Holanda <olavohcn@gmail.com>
2017-06-30 17:45:28 -0300

    Created constant file with all possible roles.

---
+++ src/utils/roles.js
@@ -0,0 +1,6 @@
+'use strict';
+
+export const ROLE_ADMIN = 'ROLE_ADMIN';
+export const ROLE_MANAGER = 'ROLE_MANAGER';
+export const ROLE_RESIDENT = 'ROLE_RESIDENT';
+export const ROLE_DOORMAN = 'ROLE_DOORMAN';
