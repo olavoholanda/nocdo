1d8b6bd3ee0cb2ae08fd86ae0b8da0006b01662e
Olavo Holanda <olavohcn@gmail.com>
2017-06-30 22:06:13 -0300

    Refactored components package and views package.

--- src/views/AnnounceView.js
+++ src/views/AnnounceView.js
@@ -1,14 +1,20 @@
 import React from "react";
 import HeaderToolBar from "../components/common/HeaderToolBar";
+import AnnounceListExpanded from "../components/annoucement/AnnounceListExpanded";
+import AnnounceForm from "../components/annoucement/AnnounceForm";
+import {Route, Redirect, Switch} from "react-router-dom";
 
 class AnnounceView extends React.Component {
-
     render() {
-
         return (
             <div>
-                <HeaderToolBar title="Avisos" icon="announcement"/>
-                <h3>Announce View</h3>
+                <HeaderToolBar link="/announce" title="Avisos" icon="announcement"/>
+                <Switch>
+                    <Route exact path={"/announce/list"} component={AnnounceListExpanded}/>
+                    <Route path={"/announce/save"} component={AnnounceForm}/>
+                    <Route path={"/announce/edit/:id"} component={AnnounceForm}/>
+                    <Redirect exact from="/announce" to="/announce/list"/>
+                </Switch>
             </div>
         );
     }
