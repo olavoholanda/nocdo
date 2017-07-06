1e963335e725865e35065e4305e189bc48fe2c45
Olavo Holanda <olavohcn@gmail.com>
2017-07-06 11:19:11 -0300

    Added edit forum message feature

--- src/components/forum/MessageFooter.js
+++ src/components/forum/MessageFooter.js
@@ -2,28 +2,61 @@ import React from "react";
 import {Toolbar, ToolbarGroup} from "material-ui/Toolbar";
 import FontIcon from "material-ui/FontIcon";
 import {grey500} from "material-ui/styles/colors";
-import Checkbox from "material-ui/Checkbox";
+import IconButton from "material-ui/IconButton";
+
+
+const styleAdminTools = {
+
+};
 
 class MessageFooter extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            edit: true,
+        };
+    }
+
     render() {
+
+        let adminTools;
+
+        if(this.state.edit){
+            adminTools = (
+                <div>
+                    <IconButton style={styleAdminTools} tooltip="Editar" onTouchTap={this.props.handlerFunction}>
+                        <FontIcon className="material-icons">mode_edit</FontIcon>
+                    </IconButton>
+                    <IconButton style={styleAdminTools} tooltip="Remover">
+                        <FontIcon className="material-icons">delete</FontIcon>
+                    </IconButton>
+                </div>
+                );
+        }
+
         return (
             <Toolbar style={{backgroundColor:"transparent", color: grey500}}>
                 <ToolbarGroup firstChild={true}>
                     <span>Postado em 15/07/2017</span>
                 </ToolbarGroup>
-                <ToolbarGroup>
+                {/*<ToolbarGroup>*/}
                     {/*<Checkbox*/}
-                    {/*checkedIcon={<FontIcon style={{color: cyan500, fontSize: 20}} className="material-icons">thumb_up</FontIcon>}*/}
-                    {/*uncheckedIcon={<FontIcon style={{color: grey500, fontSize: 20}} className="material-icons">thumb_up</FontIcon>}*/}
-                    {/*inputStyle={{width:"100px"}}*/}
-                    {/*labelPosition="left"*/}
-                    {/*label={*/}
-                    {/*<div>*/}
-                    {/*<span style={{color: grey500}}>12</span>*/}
-                    {/*<span style={{color: grey500, marginRight: 10}}>&nbsp;curtidas</span>*/}
-                    {/*</div>*/}
-                    {/*}*/}
+                        {/*checkedIcon={<FontIcon style={{color: cyan500, fontSize: 20}} className="material-icons">thumb_up</FontIcon>}*/}
+                        {/*uncheckedIcon={<FontIcon style={{color: grey500, fontSize: 20}} className="material-icons">thumb_up</FontIcon>}*/}
+                        {/*inputStyle={{width:"100px"}}*/}
+                        {/*labelPosition="left"*/}
+                        {/*label={*/}
+                            {/*<div>*/}
+                                {/*<span style={{color: grey500}}>12</span>*/}
+                                {/*<span style={{color: grey500, marginRight: 10}}>&nbsp;curtidas</span>*/}
+                            {/*</div>*/}
+                        {/*}*/}
                     {/*/>*/}
+                {/*</ToolbarGroup>*/}
+                <ToolbarGroup>
+                    {adminTools}
                 </ToolbarGroup>
             </Toolbar>
         );
--- src/components/forum/MessageListItem.js
+++ src/components/forum/MessageListItem.js
@@ -2,8 +2,53 @@ import React from "react";
 import {blueGrey50} from "material-ui/styles/colors";
 import {Card, CardHeader, CardText} from "material-ui/Card";
 import MessageFooter from "./MessageFooter";
+import RaisedButton from "material-ui/RaisedButton";
+import TextField from "material-ui/TextField";
+
+const messageText = "Com seus altos muros, portaria equipada e uma proposta de maior segurança e melhor qualidade de vida, os chamados" +
+    "“condomínios fechados” têm sido, cada vez mais, a opção de moradia das famílias das classes média e alta brasileiras." +
+    "Os grandes loteamentos, com casas confortáveis, quintais amplos e farta área de lazer, são encontrados nos arredores" +
+    "de praticamente todas as grandes e médias cidades. O que vocês acham?";
 
 class MessageListItem extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            name: "Ryan Atwood",
+            role: 'Morador',
+            username: "ryan",
+            text: messageText,
+            editText: messageText,
+            edit: false,
+        };
+    }
+
+    handleTextChange = (e, newValue) => {
+        e.preventDefault();
+        this.setState({editText: newValue});
+    };
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        let text = this.state.editText.trim();
+        if (!text) {
+            return;
+        }
+        // TODO: send request to the server
+        console.log('updating message');
+        this.setState({text: text, editText: text, edit: false});
+    };
+
+    onTouchEdit = () => {
+        this.setState({edit: true});
+    };
+
+    onTouchCancelEdit = () => {
+        this.setState({edit: false, editText: this.state.text});
+    };
+
     render() {
 
         let stripe = {};
@@ -12,20 +57,52 @@ class MessageListItem extends React.Component {
             stripe = {backgroundColor: blueGrey50};
         }
 
+        let body;
+
+        if(this.state.edit){
+            body = (
+                <CardText style={{paddingBottom: 50}}>
+                    <form onSubmit={this.handleSubmit}>
+                        <TextField floatingLabelText="Mensagem: "
+                                   floatingLabelFixed={true}
+                                   value={this.state.editText}
+                                   onChange={this.handleTextChange}
+                                   fullWidth={true}
+                                   multiLine={true}
+                                   required
+                        /><br />
+                        <RaisedButton
+                            style={{float: 'left'}}
+                            label="Descartar"
+                            onTouchTap={this.onTouchCancelEdit}
+                        />
+                        <RaisedButton
+                            style={{float: 'right'}}
+                            label="Salvar"
+                            type="submit"
+                            value="Post"
+                            primary={true}
+                        />
+                    </form>
+                </CardText>
+            );
+        } else {
+            body = (
+                <CardText>
+                    {this.state.text}
+                    <MessageFooter handlerFunction={this.onTouchEdit}/>
+                </CardText>
+            );
+        }
+
         return (
             <Card style={stripe}>
                 <CardHeader
-                    title="Ryan Atwood"
-                    subtitle="Morador"
-                    avatar="/avatars/ryan.jpg"
+                    title={this.state.name}
+                    subtitle={this.state.role}
+                    avatar={"/avatars/" + this.state.username + ".jpg"}
                 />
-                <CardText>
-                    Com seus altos muros, portaria equipada e uma proposta de maior segurança e melhor qualidade de vida, os chamados
-                    “condomínios fechados” têm sido, cada vez mais, a opção de moradia das famílias das classes média e alta brasileiras.
-                    Os grandes loteamentos, com casas confortáveis, quintais amplos e farta área de lazer, são encontrados nos arredores
-                    de praticamente todas as grandes e médias cidades. O que vocês acham?
-                    <MessageFooter/>
-                </CardText>
+                {body}
             </Card>
         );
     }
