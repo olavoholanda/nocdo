3ad8c96a7f32e8c9326326123fad1620c0c0279e
Olavo Holanda <olavohcn@gmail.com>
2017-07-12 16:33:43 -0300

    Added the path to a new poll.

--- src/views/PollView.js
+++ src/views/PollView.js
@@ -1,6 +1,7 @@
 import React from "react";
 import HeaderToolBar from "../components/common/HeaderToolBar";
 import PollList from "../components/poll/PollList";
+import PollForm from "../components/poll/PollForm";
 import {Route, Redirect, Switch} from "react-router-dom";
 
 class ForumView extends React.Component {
@@ -12,6 +13,7 @@ class ForumView extends React.Component {
                 <HeaderToolBar link="/poll" title="Enquetes" icon="view_list"/>
                 <Switch>
                     <Route exact path={"/poll/list"} component={PollList}/>
+                    <Route exact path={"/poll/new"} component={PollForm}/>
                     <Redirect exact from="/poll" to="/poll/list"/>
                 </Switch>
             </div>
